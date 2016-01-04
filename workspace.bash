63e132193a85ac2d528b0c5b35ec472dea8cac9d
Gregory Kman <greg.kman@uptake.com>
2016-01-04 13:51:17 -0600

    stream-lined nifi alias, add os detection workspace

---
+++ workspace.bash
@@ -0,0 +1,14 @@
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+   platform='freebsd'
+fi
+
+
+if [[ $platform == 'linux' ]]; then
+   alias ls='ls --color=auto'
+elif [[ $platform == 'freebsd' ]]; then
+   alias ls='ls -G'
+fi
